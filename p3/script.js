f1f68d1275a6c9c62e4be4a670015aa2598f816a
Rizky Pratama <rizkypra1156@gmail.com>
2026-09-10 14:45:42 +0700

    pertemuan 3

---
+++ p3/script.js
@@ -0,0 +1,29 @@
+// alert('selamat datang di pertemuan ke-3');
+// console.log('hello world');
+
+// let nama = 'kikuy';
+// var nim = 123250061;
+// const prodi = 'informatika';
+
+// var test = true + true
+// console.log(test);
+
+let nilai = 50;
+
+let mobil = ["honda", "toyota", "mazda"];
+
+
+// if (nilai > 76) {
+//     alert("lulus")
+// } else{
+//     alert("tidak lulus")
+// }
+
+for (let i = 0; i <= mobil.length; i++) {
+   console.log((mobil[i]));
+ }
+
+function greet (){
+    alert("hallo dwk");
+}
+
